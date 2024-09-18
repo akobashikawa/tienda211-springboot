b27a1178924bb8ec57cbeff1cae8f01c6280ae53
Antonio Kobashikawa <akobashikawa@gmail.com>
2024-09-18 16:32:11 -0500

    Regresa nombre a 103 temporalmente

--- src/main/java/com/example/tienda104/ServletInitializer.java
+++ src/main/java/com/example/tienda104/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Tienda104Application.class);
+		return application.sources(Tienda103Application.class);
 	}
 
 }
